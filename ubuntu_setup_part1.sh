1f8d699611cb4350adebb041831f1138c782bf8e
nullp0inter <ian@whitehatters.org>
2016-05-31 22:16:34 -0400

    MAJOR UPDATE: add logging vim-latex, and vimrc

--- ubuntu_setup_part1.sh
+++ ubuntu_setup_part1.sh
@@ -4,55 +4,105 @@
 ##Ian's Update and Installation Script for new ubuntu systems
 ##Use this grab all software I use and install it, making any
 ##needed changes and/or config files.
-
+touch ~/ubuntu_setup.log
 
 ##########################################
 ##update repositories and upgrade software
-echo ">>Update and Upgrade system..."
+echo ">>Update and Upgrade system..." >> ~/ubuntu_setup.log
 sudo apt-get -qq update 1> /dev/null;
 sudo apt-get -qq upgrade -qfy 1> /dev/null;
 sudo apt-get -qq dist-upgrade -qfy 1> /dev/null;
-echo "done"
+echo "done" >> ~/ubuntu_setup.log
 
 #######################
 ##apt-get installations
-echo ">>Installing new software..."
-sudo apt-get -qq install -fy open-vm-tools-* gcc-multilib g++-multilib thefuck wget curl zsh python python3 vim-runtime vim-gui-common python2.7 python2.7-dev python-pip quassel-client cinnamon 1> /dev/null
-echo "done"
+echo ">>Installing new software..." >> ~/ubuntu_setup.log
+sudo apt-get -qq install -fy open-vm-tools-* gcc-multilib g++-multilib thefuck wget curl zsh python python3 vim-runtime vim-gui-common python2.7 python2.7-dev python-pip quassel-client texlive-full cinnamon 1> /dev/null
+echo "done" >> ~/ubuntu_setup.log
 
-echo ">>PIP>>Installing python requests..."
+echo ">>PIP>>Installing python requests..." >> ~/ubuntu_setup.log
 sudo pip install requests 1> /dev/null
-echo "done"
+echo "done" >> ~/ubuntu_setup.log
 echo ">>PIP>>Installing pwntools..."
 sudo -H pip install pwntools 1> /dev/null
-echo "done"
+echo "done" >> ~/ubuntu_setup.log
 
-#####################
+#################
 ##Radare2 Install
-echo ">>Git Cloning radare2..."
+echo ">>Git Cloning radare2..." >> ubuntu_setup.log
 cd ~
 git clone https://github.com/radare/radare2.git
-echo "done"
+echo "done" >> ~/ubuntu_setup.log
 
-echo ">>Installing radare2..."
+echo ">>Installing radare2..." >> ~/ubuntu_setup.log
 ./sys/install.sh
-echo "done"
+if [ -f '/usr/bin/radare2' ]
+    then
+        echo "[SUCCESS] Installed radare2" >> ~/ubuntu_setup.log
+    else
+        echo "[!!FAIL!!] Problem installing radare2" >> ~/ubuntu_setup.log
+fi
+echo "done" >> ~/ubuntu_setup.log
 
-#######################
+##############
 ##Peda Install
-echo ">>Installing peda..."
+echo ">>Installing peda..." >> ~/ubuntu_setup.log
 git clone https://github.com/longld/peda.git ~/peda
 echo "source ~/peda/peda.py" >> ~/.gdbinit
-echo "done"
+echo "done" >> ~/ubuntu_setup.log
 
 #########################################
 ##Install Scud-Cloud, ubuntu slack client
+echo ">>Installing scudcloud" >> ~/ubuntu_setup.log
 sudo apt-add-repository -y ppa:rael-gc/scudcloud 1> /dev/null
 echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections 1> /dev/null
 sudo apt-get update 1> /dev/null
 sudo apt-get install scudcloud 1> /dev/null
+if [ -f '/usr/bin/scudcloud' ]
+    then
+        echo "[SUCCESS] Installed scudcloud" >> ~/ubuntu_setup.log
+    else
+        echo "[!!FAIL!!] Problem installing scudcloud" >> ~/ubuntu_setup.log
+fi
+echo "done" >> ~/ubuntu_setup.log
 
-######################
+#########################
+##LaTeX download (vim-latex)
+echo ">>Downloading and setting up vim-latex..." >> ~/ubuntu_setup.log
+mkdir ~/tmp
+cd ~/tmp
+wget 'http://downloads.sourceforge.net/project/vim-latex/snapshots/vim-latex-1.8.23-20141116.812-gitd0f31c9.tar.gz?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fvim-latex%2Ffiles%2F&ts=1464727158&use_mirror=netcologne'
+if [ -f vim-latex* ]
+    then
+        mv vim-latex* setupArchive-vim-latex.tar
+        tar -xf setupArchive-vim-latex.tar
+        mv vim-latex* ~/.vim
+        cd ~/.vim
+        if [ -f latextags ]
+            then
+                cd
+                sudo rm -r ~/tmp
+                echo "[SUCCESS] Vim-latex setup successful" >> ~/ubuntu_setup.log
+        else
+            echo "[!!FAIL!!] Something went wrong in vim-latex setup" >> ~/ubuntu_setup.log
+        fi
+fi
+echo "done" >> ~/ubuntu_setup.log
+
+################################
+##.vimrc Setup and Configuration
+echo ">>Setting up ~/.vimrc" >> ~/ubuntu_setup.log
+if [ -f ~/.vimrc ]
+    then
+        mv ~/.vimrc ~/.vimrc.old
+        mv ./.vimrc ~
+        echo "done" >> ~/ubuntu_setup.log
+    else
+        mv ./.vimrc ~
+        echo "done" >> ~/ubuntu_setup.log
+fi
+
+###################
 ##Oh-My-Zsh Install
-echo ">>Installing Oh-My-Zsh..."
+echo ">>Installing Oh-My-Zsh..." >> ~/ubuntu_setup.log
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
--- ubuntu_setup_part2.sh
+++ ubuntu_setup_part2.sh
@@ -1,43 +1,54 @@
 #!/bin/zsh
 
-#nullp0inter's ubuntu configuration part2
-#Continues in Zsh where the last part left off
+############################################
+#Nullp0inter's ubuntu configuration part two
+#Continues in Zsh where the last part stopped
 
-##########################
+#########################
 ##Oh-My-Zsh configuration
 echo "unsetopt no_match" >> ~/.zshrc
-echo ">>Changing theme to agnoster"
+echo ">>Changing theme to agnoster" >> ubuntu_setup.log
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' ~/.zshrc
 sed -i 's/$USER%m/$USER/' ~/.oh-my-zsh/themes/agnoster.zsh-theme
 sed -i 's/%~/%c/' ~/.oh-my-zsh/themes/agnoster.zsh-theme
-echo "done"
+echo "done" >> ~/ubuntu_setup.log
 
 ######################
 ##Create Aliases File
-echo ">>Generating zsh aliases file"
-touch ~/.aliases
+echo ">>Generating zsh aliases file" >> ~/ubuntu_setup.log
+if [ -f '~/.aliases' ]
+    then
+        echo "found .alias file, backing up to ~/.aliases.orig" >> ~/ubuntu_setup.log
+        cp '~/.aliases' '~/.aliases.orig'
+        echo "Backup complete. Adding new aliases" >> ~/ubuntu_setup.log
+        echo "[!!WARNING!!] If you have a .alias file already..." >> ~/ubuntu_setup.log
+        echo "[!!WARNING!!] this can get pretty messy. Manually " >> ~/ubuntu_setup.log
+        echo "[!!WARNING!!] double-check the new aliases for    " >> ~/ubuntu_setup.log
+        echo "[!!WARNING!!] duplicates, etc." >> ~/ubuntu_setup.log
+fi
+touch ~/.aliases;
 echo "##UTILITY" >> ~/.aliases;
 echo "alias ll='ls -la'" >> ~/.aliases;
 echo "alias q='exit'" >> ~/.aliases;
 echo "alias ropg='ROPgadget'" >> ~/.aliases;
-echo "" >> ~/.aliases
-echo "##Directories" >> ~/.aliases
-echo "done"
+echo "" >> ~/.aliases;
+echo "##Directories" >> ~/.aliases;
+echo "done" >> ~/ubuntu_setup.log
 
-echo ">>Editing .zshrc to use new alias file AND to use thefuck"
+echo ">>Editing .zshrc to use new alias file AND to use thefuck" >> ~/ubuntu_setup.log
 echo "source \$HOME/.aliases" >> ~/.zshrc
 echo "eval \$(thefuck --alias)" >> ~/.zshrc
-echo "done"
+echo "done" >> ~/ubuntu_setup.log
 
-echo ">>Adding alias file to bashrc"
+echo ">>Adding alias file to bashrc" >> ~/ubunut_setup.log
 echo "[if [ -f ~/.aliases]; then" >> .bashrc
 echo "    . ~/.aliases" >> .bashrc
 echo "fi" >> .bashrc
-echo "done"
+echo "done" >> ~/ubuntu_setup.log
 
-#############################
+#################
 ##Powerline fonts
-echo ">>Installing powerline fonts"
+echo ">>Installing powerline fonts" >> ~/ubuntu_setup.log
 wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
 wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
 mkdir ~/.fonts
@@ -47,10 +58,7 @@ mkdir ~/.config/fontconfig
 mkdir ~/.config/fontconfig/conf.d/
 mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
 fc-cache -fv ~/.fonts/
-echo "done"
-
-################################
-##Set up git directories for self
+echo "done" >> ~/ubuntu_setup.log
 
 #This is just for me, feel free to change it if you'd like
 #these repos are all public though some will be useless to you i'd imagine
@@ -65,7 +73,8 @@ if [ $USER == nullp0inter ]; then
 	echo "alias github='cd ~/Documents/Github'" >> ~/.aliases
 fi
 
-############################
+################
 ##System Restart
+echo ">>Script done. RESTARTING NOW..." >> ~/ubuntu_setup.log
 echo ">>Script done. RESTARTING NOW..."
 sudo shutdown -r 0;
