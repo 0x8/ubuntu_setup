535599054a89777bac6d9a8e52d4afb34fc3721c
nullp0inter <ian@whitehatters.org>
2016-06-01 15:05:55 -0400

    Remove the log file. That was a stupid idea

--- ubuntu_setup_part1.sh
+++ ubuntu_setup_part1.sh
@@ -4,183 +4,109 @@
 ##Ian's Update and Installation Script for new ubuntu systems
 ##Use this grab all software I use and install it, making any
 ##needed changes and/or config files.
-touch ~/ubuntu_setup.log
 
 ##########################################
 ##update repositories and upgrade software
 echo ">>Update and Upgrade system..."
-echo ">>Update and Upgrade system..." >> ~/ubuntu_setup.log
 sudo apt-get -qq update 1> /dev/null;
 sudo apt-get -qq upgrade -qfy 1> /dev/null;
 sudo apt-get -qq dist-upgrade -qfy 1> /dev/null;
 echo "done"
-echo "done" >> ~/ubuntu_setup.log
 
 #######################
 ##apt-get installations
-echo ">>Installing new software..." >> ~/ubuntu_setup.log
+echo ">>Installing new software..."
 echo "[INSTALLING] open-vm-tools-*"
-echo "[INSTALLING] open-vm-tools-*" >> ~/ubuntu_setup.log
 sudo apt-get -qq install -fy open-vm-tools-*    1> /dev/null
-echo "done"
-echo "done" >> ~/ubuntu_setup.log
 echo "[INSTALLING] gcc-multilib"
-echo "[INSTALLING] gcc-multilib" >> ~/ubuntu_setup.log
 sudo apt-get -qq install -fy gcc-multilib       1> /dev/null
-echo "done"
-echo "done" >> ~/ubuntu_setup.log
 echo "[INSTALLING] g++-multilib"
 echo "[INSTALLING] g++-multilib" >> ~/ubuntu_setup.log
 sudo apt-get -qq install -fy g++-multilib       1> /dev/null
-echo "done"
-echo "done" >> ~/ubuntu_setup.log
 echo "[INSTALLING] thefuck"
-echo "[INSTALLING] thefuck" >> ~/ubuntu_setup.log
 sudo apt-get -qq install -fy thefuck            1> /dev/null
-echo "done"
-echo "done" >> ~/ubuntu_setup.log
 echo "[INSTALLING] wget"
-echo "[INSTALLING] wget" >> ~/ubuntu_setup.log
 sudo apt-get -qq install -fy wget               1> /dev/null
-echo "done"
-echo "done" >> ~/ubuntu_setup.log
 echo "[INSTALLING] curl"
-echo "[INSTALLING] curl" >> ~/ubuntu_setup.log
 sudo apt-get -qq install -fy curl               1> /dev/null
-echo "done"
-echo "done" >> ~/ubuntu_setup.log
 echo "[INSTALLING] zsh"
-echo "[INSTALLING] zsh" >> ~/ubuntu_setup.log
 sudo apt-get -qq install -fy zsh                1> /dev/null
-echo "done"
-echo "done" >> ~/ubuntu_setup.log
 echo "[INSTALLING] python"
-echo "[INSTALLING] python" >> ~/ubuntu_setup.log
 sudo apt-get -qq install -fy python             1> /dev/null
-echo "done"
-echo "done" >> ~/ubuntu_setup.log
 echo "[INSTALLING] python3"
-echo "[INSTALLING] python3" >> ~/ubuntu_setup.log
 sudo apt-get -qq install -fy python3            1> /dev/null
-echo "done"
-echo "done" >> ~/ubuntu_setup.log
 echo "[INSTALLING] vim-runtime"
-echo "[INSTALLING] vim-runtime" >> ~/ubuntu_setup.log
 sudo apt-get -qq install -fy vim-runtime        1> /dev/null
-echo "done"
-echo "done" >> ~/ubuntu_setup.log
 echo "[INSTALLING] vim-gui-common"
-echo "[INSTALLING] vim-gui-common" >> ~/ubuntu_setup.log
 sudo apt-get -qq install -fy vim-gui-common     1> /dev/null
-echo "done"
-echo "done" >> ~/ubuntu_setup.log
 echo "[INSTALLING] python2.7"
-echo "[INSTALLING] python2.7" >> ~/ubuntu_setup.log
 sudo apt-get -qq install -fy python2.7          1> /dev/null
-echo "done"
-echo "done" >> ~/ubuntu_setup.log
 echo "[INSTALLING] python2.7-dev"
-echo "[INSTALLING] python2.7-dev" >> ~/ubuntu_setup.log
 sudo apt-get -qq install -fy python2.7-dev      1> /dev/null
-echo "done"
-echo "done" >> ~/ubuntu_setup.log
 echo "[INSTALLING] python-pip"
-echo "[INSTALLING] python-pip" >> ~/ubuntu_setup.log
 sudo apt-get -qq install -fy python-pip         1> /dev/null
-echo "done"
-echo "done" >> ubuntu_setup.log
 echo "[INSTALLING] quassel-client"
-echo "[INSTALLING] quassel-client" >> ~/ubuntu_setup.log
 sudo apt-get -qq install -fy quassel-client     1> /dev/null
-echo "done"
-echo "done" >> ~/ubuntu_setup.log
 echo "[INSTALLING] texlive-full"
-echo "[INSTALLING] texlive-full" >> ~/ubuntu_setup.log
 sudo apt-get -qq install -fy texlive-full        1> /dev/null
-echo "done"
-echo "done" >> ~/ubuntu_setup.log
 echo "[INSTALLING] cinnamon"
-echo "[INSTALLING] cinnamon" >> ~/ubuntu_setup.log
 sudo apt-get -qq install -fy cinnamon           1> /dev/null
 echo "done"
-echo "done" >> ~/ubuntu_setup.log
 
 ########################################
 ##Attempt pip install of python-requests
 echo ">>PIP>>Installing python requests..."
-echo ">>PIP>>Installing python requests..." >> ~/ubuntu_setup.log
 sudo pip install requests 1> /dev/null
 echo "done"
-echo "done" >> ~/ubuntu_setup.log
 
 #################################
 ##Attempt pip install of pwntools
 echo ">>PIP>>Installing pwntools..."
-echo ">>PIP>>Installing pwntools..." >> ~/ubuntu_setup.log
 sudo -H pip install pwntools 1> /dev/null
 echo "done"
-echo "done" >> ~/ubuntu_setup.log
 
 #################
 ##Radare2 Install
 echo ">>Git Cloning radare2..."
-echo ">>Git CLoning radare2..." >> ~/ubuntu_setup.log
 cd ~
 git clone https://github.com/radare/radare2.git
 echo "done"
-echo "done" >> ~/ubuntu_setup.log
-echo ">>Installing radare2..." >> ~/ubuntu_setup.log
+echo ">>Installing radare2..."
 ~/radare2/sys/install.sh
 if [ -f '/usr/bin/radare2' ]
     then
         echo "[SUCCESS] Installed radare2"
-        echo "[SUCCESS] Installed radare2" >> ~/ubuntu_setup.log
     else
         echo "[!!FAIL!!] Problem installing radare2"
-        echo "[!!FAIL!!] Problem installing radare2" >> ~/ubuntu_setup.log
 fi
 echo "done"
-echo "done" >> ~/ubuntu_setup.log
 
 ##############
 ##Peda Install
 echo ">>Installing peda..."
-echo ">>Installing peda..." >> ~/ubuntu_setup.log
 git clone https://github.com/longld/peda.git ~/peda
 echo "source ~/peda/peda.py" >> ~/.gdbinit
 echo "done"
-echo "done" >> ~/ubuntu_setup.log
 
 #########################################
 ##Install Scud-Cloud, ubuntu slack client
 echo ">>Installing scudcloud"
-echo ">>Installing scudcloud" >> ~/ubuntu_setup.log
 sudo apt-add-repository -y ppa:rael-gc/scudcloud 1> /dev/null
 echo "Running debconf stuff for scudcloud"
-echo "Runnong debconf stuff for scudcloud" >> ~/ubuntu_setup.log
 echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections 1> /dev/null
-echo "done"
-echo "done" >> ~/ubuntu_setup.log
 echo "[UPDATING] Running apt-get update"
-echo "[UPDATING] Running apt-get update" >> ~/ubuntu_setup.log
 sudo apt-get update
 echo "done"
-echo "done" >> ~/ubuntu_setup.log
 echo "[INSTALLING] Running apt-get install scudcloud"
-echo "[INSTALLING] Running apt-get install scudcloud" >> ~/ubuntu_setup.log
 sudo apt-get install scudcloud
 echo "done"
-echo "done" >> ~/ubuntu_setup.log
 if [ -f '/usr/bin/scudcloud' ]
     then
         echo "[SUCCESS] Installed scudcloud"
-        echo "[SUCCESS] Installed scudcloud" >> ~/ubuntu_setup.log
     else
         echo "[!!FAIL!!] Problem installing scudcloud"
-        echo "[!!FAIL!!] Problem installing scudcloud" >> ~/ubuntu_setup.log
 fi
 echo "done"
-echo "done" >> ~/ubuntu_setup.log
 
 #########################
 ##LaTeX download (vim-latex)
@@ -200,34 +126,27 @@ if [ -f vim-latex* ]
                 cd
                 sudo rm -r ~/tmp
                 echo "[SUCCESS] Vim-latex setup successful"
-                echo "[SUCCESS] Vim-latex setup successful" >> ~/ubuntu_setup.log
         else
             echo "[!!FAIL!!] Something went wrong in vim-latex setup"
-            echo "[!!FAIL!!] Something went wrong in vim-latex setup" >> ~/ubuntu_setup.log
         fi
 fi
 echo "done"
-echo "done" >> ~/ubuntu_setup.log
 
 ################################
 ##.vimrc Setup and Configuration
 echo ">>Settomg up ~/.vimrc"
-echo ">>Setting up ~/.vimrc" >> ~/ubuntu_setup.log
 cd ~/ubuntu_setup
 if [ -f ~/.vimrc ]
     then
         mv ~/.vimrc ~/.vimrc.old
         mv ./.vimrc ~
         echo "done"
-        echo "done" >> ~/ubuntu_setup.log
     else
         mv ./.vimrc ~
         echo "done"
-        echo "done" >> ~/ubuntu_setup.log
 fi
 
 ###################
 ##Oh-My-Zsh Install
 echo ">>Installing Oh-My-Zsh..."
-echo ">>Installing Oh-My-Zsh..." >> ~/ubuntu_setup.log
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
--- ubuntu_setup_part2.sh
+++ ubuntu_setup_part2.sh
@@ -4,38 +4,28 @@
 #Nullp0inter's ubuntu configuration part two
 #Continues in Zsh where the last part stopped
 echo "done"
-echo "done" >> ~/ubuntu_setup.log
 
 #########################
 ##Oh-My-Zsh configuration
 echo "unsetopt no_match" >> ~/.zshrc
 echo ">>Changing theme to agnoster"
-echo ">>Changing theme to agnoster" >> ubuntu_setup.log
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' ~/.zshrc
 sed -i 's/$USER%m/$USER/' ~/.oh-my-zsh/themes/agnoster.zsh-theme
 sed -i 's/%~/%c/' ~/.oh-my-zsh/themes/agnoster.zsh-theme
 echo "done"
-echo "done" >> ~/ubuntu_setup.log
 
 ######################
 ##Create Aliases File
 echo ">>Generating zsh aliases file"
-echo ">>Generating zsh aliases file" >> ~/ubuntu_setup.log
 if [ -f '~/.aliases' ]
     then
         echo "found existing .alias file, backing up to ~/.aliases.orig"
-        echo "found existing .alias file, backing up to ~/.aliases.orig" >> ~/ubuntu_setup.log
         cp '~/.aliases' '~/.aliases.orig'
         echo "Backup complete. Adding new aliases"
         echo "[!!WARNING!!] If you have a .alias file already..."
         echo "[!!WARNING!!] this can get pretty messy. Manually "
         echo "[!!WARNING!!] double-check the new aliases for    "
         echo "[!!WARNING!!] duplicates, etc."
-        echo "Backup complete. Adding new aliases" >> ~/ubuntu_setup.log
-        echo "[!!WARNING!!] If you have a .alias file already..." >> ~/ubuntu_setup.log
-        echo "[!!WARNING!!] this can get pretty messy. Manually " >> ~/ubuntu_setup.log
-        echo "[!!WARNING!!] double-check the new aliases for    " >> ~/ubuntu_setup.log
-        echo "[!!WARNING!!] duplicates, etc." >> ~/ubuntu_setup.log
 fi
 touch ~/.aliases;
 echo "##UTILITY" >> ~/.aliases;
@@ -45,27 +35,21 @@ echo "alias ropg='ROPgadget'" >> ~/.aliases;
 echo "" >> ~/.aliases;
 echo "##Directories" >> ~/.aliases;
 echo "done"
-echo "done" >> ~/ubuntu_setup.log
 
 echo ">>Editing .zshrc to use new alias file AND to use thefuck"
-echo ">>Editing .zshrc to use new alias file AND to use thefuck" >> ~/ubuntu_setup.log
 echo "source \$HOME/.aliases" >> ~/.zshrc
 echo "eval \$(thefuck --alias)" >> ~/.zshrc
 echo "done"
-echo "done" >> ~/ubuntu_setup.log
 
 echo ">>Adding alias file to bashrc"
-echo ">>Adding alias file to bashrc" >> ~/ubunut_setup.log
 echo "[if [ -f ~/.aliases]; then" >> .bashrc
 echo "    . ~/.aliases" >> .bashrc
 echo "fi" >> .bashrc
 echo "done"
-echo "done" >> ~/ubuntu_setup.log
 
 #################
 ##Powerline fonts
 echo ">>Installing powerline fonts"
-echo ">>Installing powerline fonts" >> ~/ubuntu_setup.log
 wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
 wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
 mkdir ~/.fonts
@@ -76,7 +60,6 @@ mkdir ~/.config/fontconfig/conf.d/
 mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
 fc-cache -fv ~/.fonts/
 echo "done"
-echo "done" >> ~/ubuntu_setup.log
 
 #This is just for me, feel free to change it if you'd like
 #these repos are all public though some will be useless to you i'd imagine
@@ -93,6 +76,5 @@ fi
 
 ################
 ##System Restart
-echo ">>Script done. RESTARTING NOW..." >> ~/ubuntu_setup.log
 echo ">>Script done. RESTARTING NOW..."
 sudo shutdown -r 0;
